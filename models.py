9239bcb64077d42ea0ca0244a6d167d1dd852ea5
Lexin Chen <110304515+lexin-chen@users.noreply.github.com>
2023-04-19 15:02:13 -0400

    updated functions

--- models.py
+++ models.py
@@ -8,51 +8,51 @@ class Models:
         self.gamma = kwargs.get("gamma", None)
         self.xi = kwargs.get("xi", None)
 
-    def model_1(self):
-        return p * np.sqrt(((self.gamma * self.ie + ea) ** 2 / ((2 * self.xi * (1 + self.gamma) ** 2) * \
-            (self.ie - ea)) - 2494.35) / (self.v_m)) + b
+    def model_1(self, p, b):
+        return p * np.sqrt(((self.gamma * self.ie + self.ea) ** 2 / ((2 * self.xi * (1 + self.gamma) ** 2) * \
+            (self.ie - self.ea)) - 2494.35) / (self.v_m)) + b
         
-    def model_2(self):
-        return p * np.sqrt(((self.ie + ea) ** 2 / (8 * self.xi * (self.ie - ea)) - 2494.35) / \
+    def model_2(self, p, b):
+        return p * np.sqrt(((self.ie + self.ea) ** 2 / (8 * self.xi * (self.ie - self.ea)) - 2494.35) / \
             (self.v_m)) + b
         
-    def model_3(self):
-        return p * np.sqrt(((self.gamma * self.ie + ea) ** 2 / ((2 * (1 + self.gamma) ** 2) * \
-            (self.ie - ea)) - 2494.35) / (self.v_m)) + b
+    def model_3(self, p, b):
+        return p * np.sqrt(((self.gamma * self.ie + self.ea) ** 2 / ((2 * (1 + self.gamma) ** 2) * \
+            (self.ie - self.ea)) - 2494.35) / (self.v_m)) + b
 
-    def model_4(self):
-        return p * np.sqrt(((self.ie + ea) ** 2 / (8 * (self.ie - ea)) * (1 + ((self.ie + ea) / \
-            (6 * (self.ie - ea) ** 3) * h)) - 2494.35) / (self.v_m)) + b
+    def model_4(self, p, b, h):
+        return p * np.sqrt(((self.ie + self.ea) ** 2 / (8 * (self.ie - self.ea)) * (1 + ((self.ie + self.ea) / \
+            (6 * (self.ie - self.ea) ** 3) * h)) - 2494.35) / (self.v_m)) + b
 
-    def model_5(self):
-        return p * np.sqrt(((self.ie + ea) ** 2 / (8 * (self.ie - ea)) * (1 + ((self.ie + ea) /\
-            (6 * (self.ie - ea) ** 2) * a)) - 2494.35) / (self.v_m)) + b
+    def model_5(self, p, b, a):
+        return p * np.sqrt(((self.ie + self.ea) ** 2 / (8 * (self.ie - self.ea)) * (1 + ((self.ie + self.ea) /\
+            (6 * (self.ie - self.ea) ** 2) * a)) - 2494.35) / (self.v_m)) + b
 
-    def model_6(self):
-        return p * np.sqrt(((self.gamma * self.ie + ea) ** 2 / (2 * self.xi * (1 + self.gamma) ** 2 * \
-            (self.ie - ea)) * (1 + ((self.gamma * self.ie + ea) / (3 * (1 + self.gamma) * self.xi ** 3 * \
-            (self.ie - ea) ** 3) * h)) - 2494.35) / (self.v_m)) + b
+    def model_6(self, p, b, h):
+        return p * np.sqrt(((self.gamma * self.ie + self.ea) ** 2 / (2 * self.xi * (1 + self.gamma) ** 2 * \
+            (self.ie - self.ea)) * (1 + ((self.gamma * self.ie + self.ea) / (3 * (1 + self.gamma) * self.xi ** 3 * \
+            (self.ie - self.ea) ** 3) * h)) - 2494.35) / (self.v_m)) + b
 
-    def model_7(self):
-        return p * np.sqrt(((self.gamma * self.ie + ea) ** 2 / (2 * self.xi * (1 + self.gamma) ** 2 * \
-            (self.ie - ea)) * (1 + ((self.gamma * self.ie + ea) / (3 * (1 + self.gamma) * self.xi ** 2 * \
-            (self.ie - ea) ** 2) * a)) - 2494.35) / (self.v_m)) + b
+    def model_7(self, p, b, a):
+        return p * np.sqrt(((self.gamma * self.ie + self.ea) ** 2 / (2 * self.xi * (1 + self.gamma) ** 2 * \
+            (self.ie - self.ea)) * (1 + ((self.gamma * self.ie + self.ea) / (3 * (1 + self.gamma) * self.xi ** 2 * \
+            (self.ie - self.ea) ** 2) * a)) - 2494.35) / (self.v_m)) + b
 
-    def model_8(self):
-        return p * np.sqrt(((self.ie + ea) ** 2 / (8 * self.xi * (self.ie - ea)) * (1 + ((self.ie + ea) / \
-            (6 * self.xi ** 3 * (self.ie - ea) ** 3) * h)) - 2494.35) / (self.v_m)) + b
+    def model_8(self, p, b, h):
+        return p * np.sqrt(((self.ie + self.ea) ** 2 / (8 * self.xi * (self.ie - self.ea)) * (1 + ((self.ie + self.ea) / \
+            (6 * self.xi ** 3 * (self.ie - self.ea) ** 3) * h)) - 2494.35) / (self.v_m)) + b
 
-    def model_9(self):
-        return p * np.sqrt(((self.gamma * self.ie + ea) ** 2 / (8 * self.xi * (self.ie - ea)) * (1 + ((self.ie + ea) / \
-            (6 * self.xi ** 2 * (self.ie - ea) ** 2) * a)) - 2494.35) / (self.v_m)) + b
+    def model_9(self, p, b, a):
+        return p * np.sqrt(((self.gamma * self.ie + self.ea) ** 2 / (8 * self.xi * (self.ie - self.ea)) * (1 + ((self.ie + self.ea) / \
+            (6 * self.xi ** 2 * (self.ie - self.ea) ** 2) * a)) - 2494.35) / (self.v_m)) + b
         
-    def model_10(self):
-        return p * np.sqrt(((self.gamma * self.ie + ea) ** 2 / (2 * (1 + self.gamma) ** 2 * \
-            (self.ie - ea)) * (1 + ((self.gamma * self.ie + ea) / (3 * (1 + self.gamma) * \
-            (self.ie - ea) ** 3) * h)) - 2494.35) / (self.v_m)) + b
+    def model_10(self, p, b, h):
+        return p * np.sqrt(((self.gamma * self.ie + self.ea) ** 2 / (2 * (1 + self.gamma) ** 2 * \
+            (self.ie - self.ea)) * (1 + ((self.gamma * self.ie + self.ea) / (3 * (1 + self.gamma) * \
+            (self.ie - self.ea) ** 3) * h)) - 2494.35) / (self.v_m)) + b
 
-    def model_11(self):
-        return p * np.sqrt(((self.gamma * self.ie + ea) ** 2 / (2 * (1 + self.gamma) ** 2 * \
-            (self.ie - ea)) * (1 + ((self.gamma * self.ie + ea) / (3 * (1 + self.gamma) * \
-            (self.ie - ea) ** 2) * a)) - 2494.35) / (self.v_m)) + b
+    def model_11(self, p, b, a):
+        return p * np.sqrt(((self.gamma * self.ie + self.ea) ** 2 / (2 * (1 + self.gamma) ** 2 * \
+            (self.ie - self.ea)) * (1 + ((self.gamma * self.ie + self.ea) / (3 * (1 + self.gamma) * \
+            (self.ie - self.ea) ** 2) * a)) - 2494.35) / (self.v_m)) + b
        
