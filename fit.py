a30c1f1126748ad50bfd9549e17ed2cd6413dbd1
Lexin Chen <110304515+lexin-chen@users.noreply.github.com>
2023-04-19 18:29:32 -0400

    updated files

--- fit.py
+++ fit.py
@@ -9,6 +9,10 @@ from rdkit.Chem import Draw
 from rdkit.Chem.Draw import rdMolDraw2D
 from adjustText import adjust_text
 import os
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+from sklearn.model_selection import LeaveOneOut, cross_val_score
+from sklearn.linear_model import LinearRegression
+from tabulate import tabulate
 
 def sep_by_dipole(file_name, df=None):
     if not df:
@@ -16,8 +20,8 @@ def sep_by_dipole(file_name, df=None):
     low_dipole = df[df["Dipole"] < 5]
     high_dipole = df[df["Dipole"] >= 5]
 
-    low_dipole.to_csv("low_dipole.csv", index=False)
-    high_dipole.to_csv("high_dipole.csv", index=False)
+    low_dipole.to_csv("low_dipoles.csv", index=False)
+    high_dipole.to_csv("high_dipoles.csv", index=False)
 
 def structure_annotation(smiles_list, exp_list, fit_data, ax):
     mol_list = [Chem.MolFromSmiles(smiles) for smiles in smiles_list]
@@ -34,7 +38,7 @@ def structure_annotation(smiles_list, exp_list, fit_data, ax):
         ax.imshow(image, extent=[x+x_offset-0.3, x+x_offset+0.3, y-0.3, y+0.3])
     return ax
 
-def loop_models_plot(df, exp, new, save_dir_path):
+def loop_models_plot(df, exp, text, save_dir_path):
     for i in range(1, 12):
         func = getattr(models, f"model_{i}")
         parameters, covariance = curve_fit(func, (df['HOMO'], df['LUMO'], df['V_m'], df['chi'], df['eta']), 
@@ -51,14 +55,66 @@ def loop_models_plot(df, exp, new, save_dir_path):
         for j, txt in enumerate(df["formula"]):
             texts.append(ax.text(exp[j], fit_data[j], txt, ha='center', va='center'))
         adjust_text(texts, ax=ax)
-        ax.title.set_text(f"Model {i}: {new}")
+        ax.title.set_text(f"Model {i}: {text}")
         ax.set_xlabel("Experimental Values")
         ax.set_ylabel("Theoretical Values")
         figure_name = f"model{i}"
         fig.savefig(f"{save_dir_path}/{figure_name}", bbox_inches = "tight", dpi = 500, transparent = True)
         print(f"Finished with Model {i}...")
-    
-def print_stats(formula, parameters, SE, mean_abs_err, rmse, loocv, aicc):
+
+def get_errors(exp, fit_data, parameters):
+    """ Calculates the mean absolute error (MAE), root-mean square error (RMSE), Akaike Information Criterion with correction for small samples (AICC),
+    and RMSE of leave-one-out cross-validation (LOOCV) for a given property and its corresponding fit data.
+    Args:
+        exp (pandas Series): A 1-dimensional numpy array containing the actual property values.
+        fit_data (np.ndarray): A 1-dimensional numpy array containing the fit data.
+        parameters (np.ndarray): A 1-dimensional numpy array containing the parameters used for fitting from `curve_fit`.
+    Returns:
+        Tuple [float, float, float, float]: A tuple containing the MAE, RMSE, LOOCV RMSE and AICC, respectively.
+    """
+    mean_abs_err = mean_absolute_error(exp, fit_data)
+    rmse = np.sqrt(mean_squared_error(exp, fit_data))
+    aicc = calculate_aicc(exp.shape[0], rmse ** 2, parameters.shape[0]) 
+    X = pd.DataFrame(fit_data)
+    Y= pd.DataFrame(exp)
+    cv = LeaveOneOut()
+    model = LinearRegression()  
+    scores = cross_val_score(model, X, Y, scoring='neg_mean_squared_error', cv=cv, n_jobs=-1)
+    loocv = np.sqrt(np.mean(np.absolute(scores)))
+    return mean_abs_err, rmse, loocv, aicc
+
+def calculate_aicc(n, mse, num_params):
+    """Calculates the Akaike information criterion (AIC) with small sample correction
+
+    Args:
+    n (int): number of observations
+    mse (float): mean square error
+    num_params (int): number of parameters
+
+    Returns:
+    aic_c (float): value for AIC corrected for small samples
+    """
+    aic = n * np.log(mse) + 2 * num_params
+    aic_c = aic + (2 * num_params * (num_params + 1))/(n - num_params - 1)
+    return (aic_c)
+
+def replace_aicc(aicc_list, stats_result):
+    """ Replace AICc values in stats_result DataFrame with the difference from the minimum AICc value.
+    Args:
+        aicc_list (list): AICc values for each regression model.
+        stats_result (pandas.DataFrame): Results summary DataFrame with model statistics.
+    Returns:
+        stats_result (pandas.DataFrame): Updated DataFrame replaced with the AICc differences or dAICc.
+    """
+    if len(aicc_list) == 0:
+        aicc_min = np.nan
+    else:
+        aicc_min = np.min(aicc_list)
+    daicc_list = list(map(lambda v: v - aicc_min, aicc_list))
+    stats_result.iloc[3]= ['%.2f' % elem for elem in daicc_list]
+    return stats_result
+
+def print_stats(model, parameters, SE, mean_abs_err, rmse, loocv, aicc):
     """ Prints statistical results for a given formula, including mean absolute error (MAE), root-mean-square error (RMSE),
     leave-one-out cross-validation (LOOCV), and difference in Akaike information criterion corrected(dAICc).
     Args:
@@ -73,33 +129,55 @@ def print_stats(formula, parameters, SE, mean_abs_err, rmse, loocv, aicc):
         stats_df (pandas.DataFrame): A DataFrame of the calculated statistics for the given formula.
     """   
     stats_dict = {
-        "Model 1": ["MAE", "RMSE", "LOOCV", "dAICc", "p", "b", "γ"],
-        "Model 2": ["MAE", "RMSE", "LOOCV", "dAICc", "p", "b", "ξ"],
-        "Model 3": ["MAE", "RMSE", "LOOCV", "dAICc", "p", "b", "γ"],
+        "Model 1": ["MAE", "RMSE", "LOOCV", "dAICc", "p", "b"],
+        "Model 2": ["MAE", "RMSE", "LOOCV", "dAICc", "p", "b"],
+        "Model 3": ["MAE", "RMSE", "LOOCV", "dAICc", "p", "b"],
         "Model 4": ["MAE", "RMSE", "LOOCV", "dAICc", "p", "b", "h"],
         "Model 5": ["MAE", "RMSE", "LOOCV", "dAICc", "p", "b", "a"],
-        "Model 6": ["MAE", "RMSE", "LOOCV", "dAICc", "p", "b", "γ", "ξ", "h"],
-        "Model 7": ["MAE", "RMSE", "LOOCV", "dAICc", "p", "b", "γ", "ξ", "a"],
-        "Model 8": ["MAE", "RMSE", "LOOCV", "dAICc", "p", "b","ξ", "h"],
-        "Model 9": ["MAE", "RMSE", "LOOCV", "dAICc", "p", "b" , "γ", "ξ", "a"],
-        "Model 10": ["MAE", "RMSE", "LOOCV", "dAICc", "p", "b", "γ", "ξ", "h"],
-        "Model 11": ["MAE", "RMSE", "LOOCV", "dAICc", "p", "b", "γ", "ξ", "a"],
+        "Model 6": ["MAE", "RMSE", "LOOCV", "dAICc", "p", "b", "h"],
+        "Model 7": ["MAE", "RMSE", "LOOCV", "dAICc", "p", "b", "a"],
+        "Model 8": ["MAE", "RMSE", "LOOCV", "dAICc", "p", "b", "h"],
+        "Model 9": ["MAE", "RMSE", "LOOCV", "dAICc", "p", "b" , "a"],
+        "Model 10": ["MAE", "RMSE", "LOOCV", "dAICc", "p", "b", "h"],
+        "Model 11": ["MAE", "RMSE", "LOOCV", "dAICc", "p", "b", "a"],
     }   
-    stats = {" ": stats_dict[formula],
-            f"{formula}": [float('%.4g' % mean_abs_err), float('%.4g' % rmse), float('%.4g' % loocv), 
+    stats = {" ": stats_dict[model],
+            f"{model}": [float('%.4g' % mean_abs_err), float('%.4g' % rmse), float('%.4g' % loocv), 
             float('%.4g' % aicc)] + [float('%.4g' % p) for p in parameters]}
 
     stats_df = pd.DataFrame(stats).set_index(" ")
     return stats_df
 
+def loop_model_table(df, exp, save_dir_path, dipole, text):
+    aicc_list = []
+    stats_list = []
+    for i in range(1, 12):
+        func = getattr(models, f"model_{i}")
+        model = f"Model {i}"
+        parameters, covariance = curve_fit(func, (df['HOMO'], df['LUMO'], df['V_m'], df['chi'], df['eta']), 
+                                        exp, maxfev = 500000)
+        fit_data = func((df['HOMO'], df['LUMO'], df['V_m'], df['chi'], df['eta']), 
+                        *parameters)
+        SE = np.sqrt(np.diag(covariance))
+        mean_abs_err, rmse, loocv, aicc = get_errors(exp, fit_data, parameters)
+        aicc_list.append(aicc)
+        stats_df = print_stats(model, parameters, SE, mean_abs_err, rmse, loocv, aicc)
+        stats_list.append(stats_df)
+    stats_result = pd.concat(stats_list, axis=1)
+    stats_result = replace_aicc(aicc_list, stats_result)
+    stats_result = stats_result.fillna("-")
+    with open(f"{save_dir_path}/{dipole}.txt", "w", encoding="utf-8") as f:
+                    f.write(f"Model {i} - {text}\n")
+                    f.write(tabulate(stats_result, headers="keys", stralign="center", tablefmt='fancy_grid'))
+        
 if __name__ == "__main__":
     dipole = "all_dipoles"
-    new = " ".join(word.capitalize() for word in dipole.split("_"))
-    save_dir_path = f"plots/{dipole}"
+    text = " ".join(word.capitalize() for word in dipole.split("_"))
+    save_dir_path = f"tables"
     if not os.path.exists(save_dir_path):
         os.makedirs(save_dir_path)
     df = pd.read_csv(f"{dipole}.csv", delimiter=",", encoding='utf-8')
     exp = df['HSP_exp']
     
-    loop_models_plot(df, exp, new, save_dir_path)
-    
+    #loop_models_plot(df, exp, text, save_dir_path)
+    #loop_model_table(df, exp)
