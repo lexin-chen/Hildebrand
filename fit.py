37289e4d0e3e069981d94672a5ca04a0499641f8
Lexin Chen <110304515+lexin-chen@users.noreply.github.com>
2023-04-19 16:44:50 -0400

    update

--- fit.py
+++ fit.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from enum import Enum
 from scipy.optimize import curve_fit
-import models
+import modules.models as models
 from matplotlib import pyplot as plt
 
 class prop_col(Enum):
